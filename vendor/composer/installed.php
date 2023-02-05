6ee28b277608617537ce283341c801a98db62e1c
Jhonny Pirela <jhosagid7@gmail.com>
2023-02-04 22:23:02 -0400

    Foler vendor

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'jhosagid/jhosagid-plugin',
+        'pretty_version' => 'dev-develop',
+        'version' => 'dev-develop',
+        'reference' => '9c98f426d9e878d132dc8b13031452077eb1b36a',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'jhosagid/jhosagid-plugin' => array(
+            'pretty_version' => 'dev-develop',
+            'version' => 'dev-develop',
+            'reference' => '9c98f426d9e878d132dc8b13031452077eb1b36a',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
